f5e3671c4ab4ef0337ca57ef8a50b12f83d52690
Pseudly <cvp999@gmail.com>
2021-08-09 02:19:54 -0400

    game is now playable

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random as rand
 from pygame.locals import *
 
 class Player:
@@ -6,6 +7,7 @@ class Player:
         self.coords = coords
         self.velocity = [0,0]
         self.acceleration = 2
+        self.p_size = 10
 
     def on_loop(self, step_size):
         self.update_velocity(self.get_move(), step_size)
@@ -39,14 +41,35 @@ class Player:
             move_string += 'right'
         return move_string
 
+    def on_render(self, display):
+        pygame.draw.circle(display,(0, 150, 255), self.coords, self.p_size)
+
+class Food():
+    def __init__(self, coords):
+        self.coords = coords
+        self.f_size = 15
+
+    def on_render(self, display):
+        pygame.draw.rect(display, (255, 0, 0), (self.coords, (self.f_size, self.f_size)))
+
 class Game:
     def __init__(self):
         self._running = True
         self.size = self.width, self.height = 1280, 720
         self._display_surf = pygame.display.set_mode(self.size)
 
+        self.score = 0
+        self.font = pygame.font.SysFont(None, 24)
+
         self.player = Player([self.width / 2, self.height / 2])
+        self.food = Food([rand.uniform(0, self.width), rand.uniform(0, self.height)])
+
         self.step_size = 0.01
+
+    #check if player is intersecting with food
+    def check_food(self):
+        distance = (((self.player.coords[0] - (self.food.coords[0] + self.food.f_size / 2)) ** 2) + ((self.player.coords[1] - (self.food.coords[1] + self.food.f_size / 2)) ** 2)) ** 0.5
+        return distance < (self.player.p_size) + (self.food.f_size / 2)
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -54,10 +77,18 @@ class Game:
 
     def on_loop(self):
         self.player.on_loop(self.step_size)
+        if self.check_food():
+            self.food = Food([rand.uniform(0, self.width), rand.uniform(0, self.height)])
+            self.score += 1
 
     def on_render(self):
         self._display_surf.fill((255, 255, 255))
-        pygame.draw.circle(self._display_surf,(0, 150, 255), self.player.coords, 10)
+
+        self.player.on_render(self._display_surf)
+        self.food.on_render(self._display_surf)
+
+        score_txt = self.font.render("Score: " + str(self.score), True, (0, 0, 0))
+        self._display_surf.blit(score_txt, (20, 20))
 
         pygame.display.update()
 
@@ -73,5 +104,6 @@ class Game:
         self.on_cleanup()
 
 if __name__ == "__main__" :
+    pygame.init()
     theApp = Game()
     theApp.on_execute()
